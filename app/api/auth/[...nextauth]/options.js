9569fc9563d03500dcf8ad96083d4fe07254f75d
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-02-28 00:33:37 +0500

    added authentication with gihub prociders

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,31 @@
+import GitHubProvider from "next-auth/providers/github";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        let userRole = "GitHub User";
+        if (profile?.email == "qamarislam1232@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_Secret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,11 @@
-"use client"
-
 import Link from 'next/link';
 import Image from 'next/image';
+import {getServerSession} from 'next-auth';
+import { options } from '@app/api/auth/[...nextauth]/options';
 
-function Nav() {
-    const isUserLoggedIn = true;
+async function Nav() {
+    const session = await getServerSession(options);
+    // console.log(session);
     return (
         <nav className='w-full flex justify-between'>
             <Link href='/' >
@@ -18,19 +19,19 @@ function Nav() {
             </Link>
             <div className='w-full flex justify-end' >
                 {
-                    isUserLoggedIn ? (
+                    session ? (
                         <div className='w-1/3 flex justify-around mt-10'>
                             <Link href='/recipes/add' className='text-blue-500 hover:text-blue-700'>Add Recipe</Link>
                             <Link href='/recipes' className='text-blue-500 hover:text-blue-700'>Recipes</Link>
                             <Link href='/profile' className='text-blue-500 hover:text-blue-700'>Profile</Link>
-                            <Link href='/logout' className='text-blue-500 hover:text-blue-700'>Logout</Link>
+                            <Link href='/api/auth/signout?callbackUrl=/' className='text-blue-500 hover:text-blue-700'>Logout</Link>
                         </div>
                     ) : (
                         <div className='w-1/3 flex justify-around  mt-10'>
                             <Link href='/recipes' className='text-blue-500 hover:text-blue-700'>Recipes</Link>
                             <Link href='/about' className='text-blue-500 hover:text-blue-700'>About Us</Link>
                             <Link href='/sign-up' className='text-blue-500 hover:text-blue-700'>Sign Up</Link>
-                            <Link href='/login' className='text-blue-500 hover:text-blue-700'>Login</Link>
+                            <Link href='/api/auth/signin' className='text-blue-500 hover:text-blue-700'>Login</Link>
                         </div>
                     )
 
