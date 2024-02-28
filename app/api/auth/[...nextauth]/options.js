7e42f771aa4c681eb206f0b5b19d65c11bf8b1ad
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-02-29 03:36:57 +0500

    added apis

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,6 @@
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from '@models/user';
 
 export const options = {
   providers: [
@@ -15,6 +17,44 @@ export const options = {
       },
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_Secret,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email:",
+          type: "text",
+          placeholder: "your-email",
+        },
+        password: {
+          label: "password:",
+          type: "password",
+          placeholder: "your-password",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const foundUser = await User.findOne({ email: credentials.email })
+            .lean()
+            .exec();
+
+          if (foundUser) {
+            console.log("User Exists");
+            const match = credentials.password == foundUser.password;
+
+            if (match) {
+              console.log("Good Pass");
+              delete foundUser.password;
+
+              foundUser["role"] = "Simple User";
+              return foundUser;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        return null;
+      },
     }),
   ],
   callbacks: {
