80fc349515358cdc0494c3e28615e83af93703f8
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-11 23:15:33 +0500

    protecting api routes

--- middleware.js
+++ middleware.js
@@ -2,7 +2,18 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-  function middleware(req) {
+  async function middleware(req) {
+    console.log(req.cookies.get("next-auth.session-token"));
+    if (req.nextUrl.pathname.startsWith('/api/recipes')) {
+      if (req.method === 'GET') {
+        return NextResponse.next();
+      }
+      if (req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE') {
+        const data = await req.json();
+        console.log("Allow the one who is creator");
+        return NextResponse.next();
+      }
+    }
     return NextResponse.next();
   },
   {
