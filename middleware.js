0a1e90dc8fdc2f4ea5bf5e610b308b6ea0e2cfa5
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-12 21:53:04 +0500

    Remove logging of session token.

--- middleware.js
+++ middleware.js
@@ -3,14 +3,11 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   async function middleware(req) {
-    console.log(req.cookies.get("next-auth.session-token"));
     if (req.nextUrl.pathname.startsWith('/api/recipes')) {
       if (req.method === 'GET') {
         return NextResponse.next();
       }
       if (req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE') {
-        const data = await req.json();
-        console.log("Allow the one who is creator");
         return NextResponse.next();
       }
     }
