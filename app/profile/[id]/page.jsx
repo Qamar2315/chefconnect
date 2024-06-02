ebff2aede31142ec73d342811c80ddc130b4671f
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-06-02 23:02:12 +0500

    some minor changes

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -23,22 +23,19 @@ function ProfilePage({ params }) {
   });
 
   const deleteRecipe = async (recipe) => {
-    // You can handle form submission here, e.g., send data to server
     try {
       const answer = confirm(`${session?.user?.name}, are you sure you want to delete the recipe?`)
       if (answer) {
         const response = await axios.delete(`/api/recipes/${recipe._id}?author_id=${recipe?.author}`);
-        // console.log(response.data);
         router.push('/recipes');
-        alert("Recipe deleted sucessfully")
+        alert("Recipe deleted successfully")
       }
     } catch (error) {
       alert('Error submitting recipe:', error)
       console.error('Error submitting recipe:', error.message);
-      // Handle submission errors appropriately, e.g., display user feedback
-
     }
   };
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -68,7 +65,7 @@ function ProfilePage({ params }) {
 
   if (loadingUser || loadingRecipes) {
     return (
-      <div className="bg-gradient-to-r from-sky-500 to-indigo-500 h-screen flex flex-col items-center justify-center">
+      <div className="bg-gray-800 h-screen flex flex-col items-center justify-center">
         <div className="container mx-auto px-4 py-16 text-center">
           <h1 className="text-4xl font-bold text-white">Loading...</h1>
         </div>
@@ -77,7 +74,7 @@ function ProfilePage({ params }) {
   }
 
   return (
-    <div className="bg-gradient-to-r from-sky-400 to-indigo-400 h-screen flex flex-col items-center justify-center">
+    <div className="bg-gray-900 h-screen flex flex-col items-center justify-center">
       <div className="container mx-auto px-4 py-16 text-center">
         <h1 className="text-4xl font-bold text-white">Profile Information</h1>
         <div className="mt-8">
@@ -85,13 +82,13 @@ function ProfilePage({ params }) {
           <p className="text-2xl text-white"><strong>Email:</strong> {userData.email}</p>
           <br />
           {session && session.user.user_id === userData._id && (
-            <Link href={`/profile/${session.user.user_id}/edit`} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-10">
+            <Link href={`/profile/${session.user.user_id}/edit`} className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded mt-10">
               Edit Profile
             </Link>
           )}
           {userRecipes.length > 0 ? (
             <div className="container mx-auto px-4 py-8">
-              <h1 className="text-3xl font-bold mb-8">{userData.name} Recipes</h1>
+              <h1 className="text-3xl font-bold mb-8 text-white">{userData.name} Recipes</h1>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {userRecipes.map((recipe) => (
                   <div key={recipe._id}>
@@ -100,8 +97,8 @@ function ProfilePage({ params }) {
                     </Link>
                     {
                       userData?._id === session?.user?.user_id && (
-                        <button onClick={() => { deleteRecipe(recipe) }} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-                          Delete Recipe?
+                        <button onClick={() => { deleteRecipe(recipe) }} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mt-2">
+                          Delete Recipe
                         </button>
                       )
                     }
